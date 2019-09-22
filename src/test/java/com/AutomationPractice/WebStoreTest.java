155a69a587399c9728e66090ac02694753b41ee5
“Anka10081” <anjuta-gu@inbox.lv>
2019-09-22 11:44:41 +0300

    Add Beforeall and Afterall

--- src/test/java/com/AutomationPractice/WebStoreTest.java
+++ src/test/java/com/AutomationPractice/WebStoreTest.java
@@ -1,19 +1,25 @@
 package com.AutomationPractice;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterAll;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WebStoreTest {
+    private static WebDriver driver;
+
+    @BeforeAll
+    public static void setUp(){
+        System.setProperty("webdriver.chrome.driver", "/Users/annaguscina/Desktop/chromedriver 3");
+        driver = new ChromeDriver();
+    }
 
     @Test
     public void webStoreTest() {
-
-        System.setProperty("webdriver.chrome.driver", "/Users/annaguscina/Desktop/chromedriver 3");
-        ChromeDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-
         driver.get("http://automationpractice.com");
 
         WebElement womenLink = driver.findElement(By.linkText("Women"));
@@ -22,4 +28,11 @@ public class WebStoreTest {
         WebElement leftBlockSubcategory = driver.findElement(By.xpath("//div[@id='categories_block_left']"));
     }
 
+    @AfterAll
+    public static void cleanUp(){
+        if (driver != null) {
+            driver.close();
+            driver.quit();
+        }
+    }
 }
